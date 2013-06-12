bbedb36bd8745156f96d440608d85b7db9828173
Sebastian Westberg <sebastianostbrink@gmail.com>
2013-06-12 22:24:10 +0200

    Added some new tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,9 +11,22 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new" do
+  test "should be redirected when not logged in" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should render the new page when logged in" do
+    sign_in users(:jason)
     get :new
     assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to login_path
   end
 
   test "should create status" do
